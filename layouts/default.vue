1a240755f7e4f3f76ffa070a03859ad6fe033059
Alex Mur <lehamuraschov2014@yandex.ru>
2024-03-18 14:09:39 +0200

    feat: create folder layouts with default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+   <Navbar/>
+    <main>
+    <div class="conteiner">
+        <nuxt />
+    </div>
+  </main>
+  </div>
+
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+
+export default {
+    components: { Navbar }
+}
+</script>
