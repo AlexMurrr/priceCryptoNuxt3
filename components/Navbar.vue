eb819c729441c31a7d3313a654dee1c7d642d7df
Alex Mur <lehamuraschov2014@yandex.ru>
2024-04-13 10:10:44 +0200

    feat: create navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,26 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+<div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <nuxt-link to="/" class="font-bold">Price crypto</nuxt-link>
+                <ul class="flex gap-4">
+                   <li><nuxt-link to="/now">Price now</nuxt-link></li>
+                   <li><nuxt-link to="/week">Weekly price chart</nuxt-link></li>
+                   <li><nuxt-link to="/month">Monthly price chart</nuxt-link></li>
+                   <li><nuxt-link to="/year">Price chart for the year</nuxt-link></li>
+                   <li><nuxt-link to="/choose">Choose your range</nuxt-link></li>
+                </ul>                
+            </nav>
+        </header>
+
+        <!-- out the page content -->
+      </div>
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
       <h1 class="navbar-brand">Price Bitcoin</h1>
       <div class="collapse navbar-collapse" id="navbarNav">
@@ -32,7 +53,7 @@
         </ul>
       </div>
     </div>
-  </nav>
+  </nav> -->
 
 
 </template>
